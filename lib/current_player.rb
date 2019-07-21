e3c703cc862ad6d12fbf453c3be0f364fcbca3bb
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-21 19:48:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,10 @@
 def turn_count(board)
   counter = 0
-  board.each do |turn|
-    if turn != " " || turn != ""
+  board.each do |position|
+    if position == "X" || position == "O"
       counter += 1
     end
-    return counter
+    counter
   end
 end
 
